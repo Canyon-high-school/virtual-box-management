aaa723b4e4f45427c3f79dc5cf1583295468618a
Lee Blackburn <lblackb@Lees-MacBook-Pro.local>
2013-12-09 07:37:12 -0500

    move images dir; change from ips to hostnames; commented out mac settings (TODO: externalize configuration); move VM name to variable; added step debugging output to know where you are in the script; added vm shutdown and start commands.

--- publish_image.sh
+++ publish_image.sh
@@ -2,32 +2,48 @@
 
 #for the ubuntu systems
 PUB_USER=workstation
-VMIMAGES_DIR=/home/${PUB_USER}/VMImages
+VMIMAGES_DIR=/home/${PUB_USER}/VMImages/images/dist
 SERVERS="\
-192.168.42.108 \
+imagemaster.local \
+lab4.local \
 "
 
 #for the mac
-PUB_USER=lblackb
-VMIMAGES_DIR=/Users/${PUB_USER}/VMImages
-SERVERS="\
-192.168.42.109 \
-"
+#PUB_USER=lblackb
+#VMIMAGES_DIR=/Users/${PUB_USER}/VMImages
+#SERVERS="\
+#192.168.42.109 \
+#"
 
 IMAGE=$1
 IMAGENAME=`basename ${IMAGE}`
-
+VMNAME="IE10.Win7.For.MacVirtualBox_WBAH"
 echo "Publish image: [${IMAGENAME}] [${IMAGE}]"
 ls -lh ${IMAGE}
 
 
 for SERVER in ${SERVERS} ; do
+    echo "~1"
     REMOTE=${PUB_USER}@${SERVER}
-    echo "Publish to ${REMOTE}"
-    ssh ${PUB_USER}@${SERVER} "mkdir -p ${VMIMAGES_DIR}"
-    scp $IMAGE ${PUB_USER}@${SERVER}:${VMIMAGES_DIR}
-    ssh ${PUB_USER}@${SERVER} "ls -lh ${VMIMAGES_DIR}/${IMAGENAME}"
-    ssh ${PUB_USER}@${SERVER} "vboxmanage import ${VMIMAGES_DIR}/${IMAGENAME}"
+    echo "~2 Publish to ${REMOTE}"
+    echo "~3 mkdir ${VMIMAGES_DIR}"
+    ssh ${REMOTE} "mkdir -p ${VMIMAGES_DIR}"
+    echo "~4 copy image"
+#    scp $IMAGE ${REMOTE}:${VMIMAGES_DIR}
+    echo "~5 list image"
+    ssh ${REMOTE} "ls -lh ${VMIMAGES_DIR}/${IMAGENAME}"
+    echo "~6a ungegister vm - shutdown"
+    ssh ${REMOTE} "vboxmanage controlvm \"${VMNAME}\" acpipowerbutton"
+## ugly hack to sleep some period before shutdown completes.  need to figure out a way to poll if the vm is shutdown.
+    echo "~6b ungegister vm - pause for shutdown"
+    sleep 10
+    echo "~6c ungegister vm"
+    ssh ${REMOTE} "vboxmanage unregistervm --delete \"${VMNAME}\""
+    echo "~7 import vm"
+    ssh ${REMOTE} "vboxmanage import ${VMIMAGES_DIR}/${IMAGENAME}"
 #    ssh ${PUB_USER}@${SERVER} "vboxmanage import --dry-run ${VMIMAGES_DIR}/${IMAGENAME}"
 #    vboxmanage import --dry-run /home/workstation/VMImages/vmout_2013-35-06_223552.ova
+
+    echo "~8 start vm"
+    ssh ${REMOTE} "vboxmanage startvm \"${VMNAME}\""
 done
